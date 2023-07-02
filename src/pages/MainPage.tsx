097af1b8a12d4373232c5d22337ea5547abfcc51
BunFlacher <viktor-slavityack2015@yandex.ru>
2023-07-02 11:19:58 +0700

    change

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -9,12 +9,15 @@ const MainPage = () => {
     }, [search])
 
     return (
-        <div className="w-[1000px] mx-auto flex justify-center mt-[15px]"> 
+        <div className="w-[600px] mx-auto flex justify-center mt-[15px] flex-wrap">
             <Input
             value={search}
             placeholder="Search Users"
             onChange={e => setSearch(e.target.value)}
             />
+            <ul className="bg-[rgb(66,66,66);] max-h-[200px] mt-[5px] w-screen">
+                <li>dasdasds</li>
+            </ul>
         </div>
     )
 }
